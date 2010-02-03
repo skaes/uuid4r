49e7a13c59a30bf2f57684d5f2f6458a38404c02
Stefan Kaes <skaes@railsexpress.de>
2010-02-03 05:56:17 +0100

    use uuid-config to figure out build flags

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,13 +1,17 @@
 require 'mkmf'
-$LDFLAGS = "-lossp-uuid"
-$CFLAGS = "-Wall"
-if !have_library('ossp-uuid')
-  puts "OSSP uuid liberary required -- not found."
+
+if File.exist?(`which uuid-config`.chomp)
+  $CFLAGS << " -Wall " << `uuid-config --cflags`.chomp
+  $LDFLAGS << " " << `uuid-config --ldflags`.chomp
+end
+
+if !have_library('uuid')
+  puts "OSSP uuid library required -- not found."
   exit 1
 end
 create_makefile('uuid4r')
 File.open("Makefile", "a") << <<-EOT
 
 check:	$(DLLIB)
-	@$(RUBY) $(srcdir)/test/test_uuid.rb 
+	@$(RUBY) #{File.dirname(__FILE__)}/../test/test_uuid.rb
 EOT
