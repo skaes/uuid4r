1d551ad069fafdcac6d4769598122c70956c8a69
Stefan Kaes <skaes@railsexpress.de>
2011-05-15 09:45:14 +0200

    hopefully fixed install problems on all platforms

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,15 +1,20 @@
 require 'mkmf'
 
+lib="uuid"
+
 if File.exist?(`which uuid-config`.chomp)
+  lib = `uuid-config --libs`.chomp.gsub(/-l/, '')
   $CFLAGS << " -Wall " << `uuid-config --cflags`.chomp
   $LDFLAGS << " " << `uuid-config --ldflags`.chomp
 end
 
-if !have_library('uuid')
-  puts "OSSP uuid library required -- not found."
+unless have_library(lib, 'uuid_export')
+  puts "OSSP uuid library '#{lib}' required -- not found."
   exit 1
 end
+
 create_makefile('uuid4r')
+
 File.open("Makefile", "a") << <<-EOT
 
 check:	$(DLLIB)
